757295b215e87eff15a7aedd5fffb03480f04961
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2026-09-05 18:24:27 +0800

    fix: restrict CORS policy and add api gw rate limiting

--- frontend/src/api/payments.ts
+++ frontend/src/api/payments.ts
@@ -4,14 +4,17 @@ import { createApi } from './utils/createApi';
 export const getTransactionDetails = (transactionDetails: TransactionDetails) =>
   createApi<GetTransactionDetailsOut>({
     method: 'post',
+    authorize: true,
     apiService: 'payments',
     url: '/transaction/fees',
     body: { ...transactionDetails }
   });
 
+
 export const createEwalletPaymentRequest = (paymentDetails: EWalletPaymentIn) =>
   createApi<PaymentRequestOut>({
     method: 'post',
+    authorize: true,
     apiService: 'payments',
     url: '/e_wallet/payment_method',
     body: { ...paymentDetails }
@@ -20,7 +23,9 @@ export const createEwalletPaymentRequest = (paymentDetails: EWalletPaymentIn) =>
 export const initiateDirectDebitPayment = (paymentDetails: DirectDebitPaymentIn) =>
   createApi<PaymentRequestOut>({
     method: 'post',
+    authorize: true,
     apiService: 'payments',
     url: '/direct_debit/payment_request',
     body: { ...paymentDetails }
   });
+
